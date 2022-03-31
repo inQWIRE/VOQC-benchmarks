912aadc852f99f45e7b97774631a738ee381e550
Kesha Hietala <kesha@cs.umd.edu>
2022-03-31 10:26:13 -0400

    Added timeout to run_pyzx and cycle_17_3 benchmark

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# VOQC Benchmarking Scripts
+# VOQC Benchmarks
 
 This directory contains scripts for evaluating the [VOQC optimizer](https://github.com/inQWIRE/SQIR). In particular, use `run_on_staq_benchmarks.sh` to generate the data from our paper.
 
 We consider the following benchmarks:
 * **Nam-benchmarks**: Used to evaluate [an optimizer by Nam et al.](https://arxiv.org/abs/1710.07345); available in Quipper format at [github.com/njross/optimizer](https://github.com/njross/optimizer). We used [PyZX](https://github.com/Quantomatic/pyzx)'s `from_quipper_file` function to convert the circuits to OpenQASM.
-* **staq-benchmarks**: Used to evaluate the [Staq compiler](https://arxiv.org/abs/1912.06070); available at [github.com/softwareQinc/staq](https://github.com/softwareQinc/staq/tree/main/examples/staq_paper/benchmarks). We exclude mod_adder_1048576.qasm and cycle_17_3.qasm because they are ill-typed. We exclude gf^X_mult.qasm for X >= 32 and hwbX.qasm for X >= 8 to keep the input circuit sizes under 10^4 gates.
+* **staq-benchmarks**: Used to evaluate the [Staq compiler](https://arxiv.org/abs/1912.06070); available at [github.com/softwareQinc/staq](https://github.com/softwareQinc/staq/tree/main/examples/staq_paper/benchmarks). We exclude mod_adder_1048576.qasm, gf^X_mult.qasm for X >= 32, and hwbX.qasm for X >= 8 to keep the input circuit sizes under 10^4 gates.
 
 ## Setup
 
--- run_pyzx.py
+++ run_pyzx.py
@@ -2,6 +2,24 @@ import pyzx as zx
 import os
 import sys
 import time
+import signal
+from contextlib import contextmanager
+
+class TimeoutException(Exception): pass
+
+# From https://stackoverflow.com/questions/366682/how-to-limit-execution-time-of-a-function-call
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+TIMEOUT = 600 # 600s = 10min
 
 def run(d, fname):
     
@@ -11,9 +29,6 @@ def run(d, fname):
     
     for fname in os.listdir(d):
 
-        if fname == "gf2^10_mult.qasm" or fname == "gf2^16_mult.qasm":
-            continue
-
         print("Processing %s..." % fname)
         
         circ = zx.Circuit.load(os.path.join(d, fname)).to_basic_gates()
@@ -22,15 +37,24 @@ def run(d, fname):
         g = circ.to_graph()
         zx.full_reduce(g)
         g.normalize()
-        opt_circ1 = zx.extract_circuit(g).to_basic_gates()
-        opt_circ2 = zx.full_optimize(opt_circ1)
+        opt_circ = zx.extract_circuit(g).to_basic_gates()
         stop = time.perf_counter()
+        elapsed = stop - start
+        if elapsed < TIMEOUT:
+            try: # try to run full_optimize, cancelling after 10 minutes - elapsed
+                with time_limit(TIMEOUT - round(elapsed)):
+                    opt_circ = zx.full_optimize(opt_circ)
+                    stop = time.perf_counter()
+            except TimeoutException:
+                print("\tfull_optimize is slow; only running full_reduce")
+        else:
+            print("\tfull_optimize is slow; only running full_reduce")
         
-        total_count = len(opt_circ2.gates)
-        two_count = opt_circ2.twoqubitcount()
-        t_count = zx.tcount(opt_circ2)
+        total_count = len(opt_circ.gates)
+        two_count = opt_circ.twoqubitcount()
+        t_count = zx.tcount(opt_circ)
 
-        print("\t Total %d, T %d, 2-qubit %d\n" % (total_count, t_count, two_count))
+        print("\tTotal %d, T %d, 2-qubit %d\n" % (total_count, t_count, two_count))
 
         f.write("%s,%d,%d,%d,%f\n" % (fname, t_count, two_count, total_count, stop - start))
 
